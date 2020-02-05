3d331962668520b674d3ec588eb1acfc8463ec75
Ashique <ashique12009@gmail.com>
2020-02-05 13:01:40 +0600

    Response of notices are printing after promise

--- src/app/api/notice.service.ts
+++ src/app/api/notice.service.ts
@@ -7,12 +7,29 @@ import { HttpClient } from '@angular/common/http';
 export class NoticeService {
 
   private baseEndPointUrl = "http://localhost:8080/wordpress/wp-json/wp/v2/";
+  public data : any;
   
   constructor(private http: HttpClient) { }
 
   getAllNotices(){
-    this.http.get(this.baseEndPointUrl + "notice").subscribe((response) => {
-      console.log(response);
+
+    return new Promise(resolve => {
+      // We're using Angular HTTP provider to request the data,
+      // then on the response, it'll map the JSON data to a parsed JS object.
+      // Next, we process the data and resolve the promise with the new data.
+      this.http.get(this.baseEndPointUrl + "notice")
+        // .map(res => res.json())
+        .subscribe(data => {
+          // we've got back the raw data, now generate the core schedule data
+          // and save the data for later reference
+          this.data = data;
+          resolve(this.data);
+        });
     });
+
+    // this.http.get(this.baseEndPointUrl + "notice").subscribe((response) => {
+    //   console.log(response);
+    //   return response;
+    // });
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,12 +8,19 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  private showHide = true;
+  private noticeList : any = [];
+
   constructor(private noticeService: NoticeService) {
     this.getAllNotice();
   }
 
   getAllNotice(){
-    this.noticeService.getAllNotices();
+    this.noticeService.getAllNotices().then(data => {
+      this.noticeList = data;
+    });
+    this.showHide = false;
+    console.log(this.noticeList);
   }
 
 }
